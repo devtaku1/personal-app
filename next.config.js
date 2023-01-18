76b010cefdc4ef6613409b8dbbc168fd6d8020df
segfault0x61 <torresj1997@outlook.com>
2023-01-18 16:58:42 -0600

    feat: add basic page layout

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    dangerouslyAllowSVG: true,
+    domains: [
+      'tailwindui.com',
+      'images.unsplash.com',
+      'eincode.com',
+      'thrangra.sirv.com',
+      'picsum.photos'
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
