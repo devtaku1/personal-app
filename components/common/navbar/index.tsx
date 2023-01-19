b585354dbdbf57b013978c805cc8903c5b2754ad
segfault0x61 <torresj1997@outlook.com>
2023-01-19 10:42:49 -0600

    refactor: move header and navbar to component

---
+++ components/common/navbar/index.tsx
@@ -0,0 +1,123 @@
+import { Popover, Transition } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Fragment } from 'react';
+
+import Image from 'next/legacy/image';
+import Link from 'next/link';
+
+import { navigation } from './navigation';
+
+const Navbar = () => {
+  return (
+    <>
+      <svg
+        className='absolute inset-y-0 right-0 hidden w-48 h-full text-white transform translate-x-1/2 lg:block'
+        fill='currentColor'
+        viewBox='0 0 100 100'
+        preserveAspectRatio='none'
+        aria-hidden='true'
+      >
+        <polygon points='50,0 100,0 50,100 0,100' />
+      </svg>
+
+      {/* NAVIGATION START */}
+      <Popover>
+        <div className='relative px-4 pt-6 sm:px-6 lg:px-8'>
+          <nav
+            className='relative flex items-center justify-between sm:h-10 lg:justify-start'
+            aria-label='Global'
+          >
+            <div className='flex items-center flex-grow flex-shrink-0 lg:flex-grow-0'>
+              <div className='flex items-center justify-between w-full md:w-auto'>
+                <Link legacyBehavior href='/'>
+                  <a>
+                    <span className='sr-only'>Your Company</span>
+                    <Image
+                      width={30}
+                      height={30}
+                      alt='Your Company'
+                      className='w-auto h-8 sm:h-10'
+                      src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
+                    />
+                  </a>
+                </Link>
+                <div className='flex items-center -mr-2 md:hidden'>
+                  <Popover.Button className='inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
+                    <span className='sr-only'>Open main menu</span>
+                    <Bars3Icon className='w-6 h-6' aria-hidden='true' />
+                  </Popover.Button>
+                </div>
+              </div>
+            </div>
+            <div className='hidden md:ml-10 md:block md:space-x-8 md:pr-4'>
+              {navigation.map((item) => (
+                <Link legacyBehavior key={item.name} href={item.href}>
+                  <a className='font-medium text-gray-500 hover:text-gray-900'>
+                    {item.name}
+                  </a>
+                </Link>
+              ))}
+            </div>
+            <div className='hidden md:block'>
+              {/* Search Input Component */}
+            </div>
+          </nav>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter='duration-150 ease-out'
+          enterFrom='opacity-0 scale-95'
+          enterTo='opacity-100 scale-100'
+          leave='duration-100 ease-in'
+          leaveFrom='opacity-100 scale-100'
+          leaveTo='opacity-0 scale-95'
+        >
+          <Popover.Panel
+            focus
+            className='absolute inset-x-0 top-0 z-10 p-2 transition origin-top-right transform md:hidden'
+          >
+            <div className='bg-white rounded-lg shadow-md ring-1 ring-black ring-opacity-5'>
+              <div className='flex items-center justify-between px-5 pt-4'>
+                <div>
+                  <Image
+                    height={20}
+                    width={20}
+                    className='w-auto h-8'
+                    src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
+                    alt=''
+                  />
+                </div>
+                <div className='-mr-2'>
+                  <Popover.Button className='inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
+                    <span className='sr-only'>Close main menu</span>
+                    <XMarkIcon className='w-6 h-6' aria-hidden='true' />
+                  </Popover.Button>
+                </div>
+              </div>
+              <div className='px-2 pt-2 pb-3 space-y-1'>
+                {navigation.map((item) => (
+                  <Link legacyBehavior key={item.name} href={item.href}>
+                    <a className='block px-3 py-2 text-base font-medium text-gray-700 rounded-md hover:bg-gray-50 hover:text-gray-900'>
+                      {item.name}
+                    </a>
+                  </Link>
+                ))}
+              </div>
+              <div className='px-4'>{/* Search Input Component */}</div>
+              <a
+                href='#'
+                className='block w-full px-5 py-3 font-medium text-center text-indigo-600 bg-gray-50 hover:bg-gray-100'
+              >
+                Log in
+              </a>
+            </div>
+          </Popover.Panel>
+        </Transition>
+      </Popover>
+      {/* NAVIGATION END */}
+    </>
+  );
+};
+
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,12 @@
 import type { NextPage } from 'next';
+
 import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/legacy/image';
-import { Fragment } from 'react';
 
-import { Popover, Transition } from '@headlessui/react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
-
-import { Footer } from '../components/common';
-
-const navigation = [
-  { name: 'Blogs', href: '/blogs' },
-  { name: 'Portfolio', href: '/portfolios' },
-];
+import Footer from '../components/common/footer';
+import Header from '../components/common/header';
+import Navbar from '../components/common/navbar';
 
 const blogs = [
   {
@@ -64,151 +58,8 @@ const Home: NextPage = () => {
 
         <div className='relative'>
           <div className='relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32'>
-            {/* Navbar Starts */}
-            <>
-              <svg
-                className='absolute inset-y-0 right-0 hidden w-48 h-full text-white transform translate-x-1/2 lg:block'
-                fill='currentColor'
-                viewBox='0 0 100 100'
-                preserveAspectRatio='none'
-                aria-hidden='true'
-              >
-                <polygon points='50,0 100,0 50,100 0,100' />
-              </svg>
-
-              {/* NAVIGATION START */}
-              <Popover>
-                <div className='relative px-4 pt-6 sm:px-6 lg:px-8'>
-                  <nav
-                    className='relative flex items-center justify-between sm:h-10 lg:justify-start'
-                    aria-label='Global'
-                  >
-                    <div className='flex items-center flex-grow flex-shrink-0 lg:flex-grow-0'>
-                      <div className='flex items-center justify-between w-full md:w-auto'>
-                        <Link legacyBehavior href='/'>
-                          <a>
-                            <span className='sr-only'>Your Company</span>
-                            <Image
-                              width={30}
-                              height={30}
-                              alt='Your Company'
-                              className='w-auto h-8 sm:h-10'
-                              src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
-                            />
-                          </a>
-                        </Link>
-                        <div className='flex items-center -mr-2 md:hidden'>
-                          <Popover.Button className='inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
-                            <span className='sr-only'>Open main menu</span>
-                            <Bars3Icon className='w-6 h-6' aria-hidden='true' />
-                          </Popover.Button>
-                        </div>
-                      </div>
-                    </div>
-                    <div className='hidden md:ml-10 md:block md:space-x-8 md:pr-4'>
-                      {navigation.map((item) => (
-                        <Link legacyBehavior key={item.name} href={item.href}>
-                          <a className='font-medium text-gray-500 hover:text-gray-900'>
-                            {item.name}
-                          </a>
-                        </Link>
-                      ))}
-                    </div>
-                    <div className='hidden md:block'>
-                      {/* Search Input Component */}
-                    </div>
-                  </nav>
-                </div>
-
-                <Transition
-                  as={Fragment}
-                  enter='duration-150 ease-out'
-                  enterFrom='opacity-0 scale-95'
-                  enterTo='opacity-100 scale-100'
-                  leave='duration-100 ease-in'
-                  leaveFrom='opacity-100 scale-100'
-                  leaveTo='opacity-0 scale-95'
-                >
-                  <Popover.Panel
-                    focus
-                    className='absolute inset-x-0 top-0 z-10 p-2 transition origin-top-right transform md:hidden'
-                  >
-                    <div className='bg-white rounded-lg shadow-md ring-1 ring-black ring-opacity-5'>
-                      <div className='flex items-center justify-between px-5 pt-4'>
-                        <div>
-                          <Image
-                            height={20}
-                            width={20}
-                            className='w-auto h-8'
-                            src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
-                            alt=''
-                          />
-                        </div>
-                        <div className='-mr-2'>
-                          <Popover.Button className='inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
-                            <span className='sr-only'>Close main menu</span>
-                            <XMarkIcon className='w-6 h-6' aria-hidden='true' />
-                          </Popover.Button>
-                        </div>
-                      </div>
-                      <div className='px-2 pt-2 pb-3 space-y-1'>
-                        {navigation.map((item) => (
-                          <Link legacyBehavior key={item.name} href={item.href}>
-                            <a className='block px-3 py-2 text-base font-medium text-gray-700 rounded-md hover:bg-gray-50 hover:text-gray-900'>
-                              {item.name}
-                            </a>
-                          </Link>
-                        ))}
-                      </div>
-                      <div className='px-4'>{/* Search Input Component */}</div>
-                      <a
-                        href='#'
-                        className='block w-full px-5 py-3 font-medium text-center text-indigo-600 bg-gray-50 hover:bg-gray-100'
-                      >
-                        Log in
-                      </a>
-                    </div>
-                  </Popover.Panel>
-                </Transition>
-              </Popover>
-              {/* NAVIGATION END */}
-            </>
-            {/* Navbar Ends */}
-
-            {/* Header Starts */}
-            <main className='px-4 mx-auto mt-10 max-w-7xl sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28'>
-              <div className='sm:text-center lg:text-left'>
-                <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl'>
-                  <span className='block xl:inline'>Welcome to my</span>{' '}
-                  <span className='block text-indigo-600 xl:inline'>
-                    little space
-                  </span>
-                </h1>
-                <p className='mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0'>
-                  My name is XY and I am a software developer. Feel free to
-                  check my working experience and blogs about tech topics.
-                </p>
-                <div className='mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start'>
-                  <div className='rounded-md shadow'>
-                    <a
-                      href='#'
-                      className='flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg'
-                    >
-                      My Portfolio
-                    </a>
-                  </div>
-                  <div className='mt-3 sm:mt-0 sm:ml-3'>
-                    <a
-                      href='#'
-                      className='flex items-center justify-center w-full px-8 py-3 text-base font-medium text-indigo-700 bg-indigo-100 border border-transparent rounded-md hover:bg-indigo-200 md:py-4 md:px-10 md:text-lg'
-                    >
-                      CV
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </main>
-            {/* Header Ends */}
+            <Navbar />
+            <Header />
           </div>
           <div className='relative lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2'>
             <Image
