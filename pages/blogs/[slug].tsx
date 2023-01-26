576db3904fd74dfd10025259e1e68654cae031eb
segfault0x61 <torresj1997@outlook.com>
2023-01-25 23:17:48 -0600

    feat: get blog by slug

--- pages/blogs/[slug].tsx
+++ pages/blogs/[slug].tsx
@@ -1,7 +1,13 @@
 import { NextPage } from 'next/types';
 import { PageLayout } from '@components/layout';
+import { getBlogBySlug, getBlogsSlugs } from '@lib/blogs';
+import { Blog } from '@interfaces/Blog';
 
-const BlogDetail: NextPage = () => {
+type Props = {
+  blog: Blog;
+};
+
+const BlogDetail: NextPage<Props> = ({ blog }) => {
   return (
     <>
       <PageLayout>
@@ -27,19 +33,19 @@ const BlogDetail: NextPage = () => {
                 <div className='ml-3'>
                   <p className='text-sm font-medium text-gray-900 !mb-0'>
                     <a href='#' className='hover:underline'>
-                      Author Name
+                      {blog.author}
                     </a>
                   </p>
                   <div className='flex space-x-1 text-sm text-gray-500'>
-                    <time dateTime='{date}'>2022-10-10</time>
+                    <time dateTime='{date}'>{blog.date}</time>
                   </div>
                 </div>
               </div>
               <div className='flex self-end'>{/* Social Links Here */}</div>
             </div>
-            <h1 className='mb-1 text-4xl font-bold'>My First Blog</h1>
+            <h1 className='mb-1 text-4xl font-bold'>{blog.title}</h1>
             <h2 className='mb-2 text-xl text-gray-600 blog-detail-header-subtitle'>
-              My first blog description
+              {blog.description}
             </h2>
             <div className='relative w-full mx-auto bg-black h-96'>
               {/* <Image
@@ -52,12 +58,26 @@ const BlogDetail: NextPage = () => {
           {/* Blog Header Ends */}
           <article className='prose lg:prose-lg markdown-image-50'>
             {/* Blog Content Here */}
-            Content Here
+            {blog.content}
           </article>
         </div>
       </PageLayout>
     </>
   );
+};
+
+export const getStaticProps = (context: any) => {
+  const { slug } = context.params;
+  const blog = getBlogBySlug(slug);
+
+  return { props: { blog } };
+};
+
+export const getStaticPaths = () => {
+  const slugs = getBlogsSlugs();
+  const paths = slugs.map((slug) => ({ params: { slug } }));
+
+  return { paths, fallback: false };
 };
 
 export default BlogDetail;
